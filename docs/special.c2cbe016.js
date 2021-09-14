86f8fc118f6a29558728e4bc3c5fb1ba9738697e
Jeff Hykin <jeff.hykin@gmail.com>
2021-09-13 21:15:25 -0500

    add d3 demo

--- docs/special.c2cbe016.js
+++ docs/special.c2cbe016.js
@@ -1136,7 +1136,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57127" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61868" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- web_server/website.jsx
+++ web_server/website.jsx
@@ -1,6 +1,23 @@
+// add some CSS
+// require("css-baseline/css/2")
+require("./static_files/materialize.css")
+// require("materialize-css")
+
+// get d3.js
+const Card = require("./code/Card")
+const d3Demo = require("./code/d3_demo")
+
 // create the body
-document.body = <body style={{display: "flex", alignItems: "center", justifyContent: "center", fontSize: "30pt", fontFamily: "sans-serif",}}>
-    
-    Hello World!
-    
-</body>
+document.body = (
+    <body style={{ display: "flex", alignItems: "center", justifyContent: "center", fontFamily: "sans-serif", flexDirection: "column", background: "whitesmoke" }}>
+        
+        <h1 style={{fontSize: "30pt",}}>
+            Hello World!
+        </h1>
+        
+        <Card title="This is a D3 Demo" style={{width: "50rem"}}>
+            {d3Demo}
+        </Card>
+        
+    </body>
+)
