071739374c72cea5814fadaf3e8914ddc8ef4430
Jeff Hykin <jeff.hykin@gmail.com>
2021-10-06 18:33:51 -0500

    add FrequencyChart example

--- web_server/code/pages/Home.jsx
+++ web_server/code/pages/Home.jsx
@@ -1,4 +1,5 @@
 let GraphDisplay = require("../skeletons/GraphDisplay")
+let FrequencyChart = require("../components/FrequencyChart")
 
 module.exports = ({ children, ...properties }) => {
     return <body class="column centered" style="height: 100vh">
@@ -13,6 +14,8 @@ module.exports = ({ children, ...properties }) => {
             <h4>Howdy7</h4>
             <h4>Howdy8</h4>
         </GraphDisplay>
-        
+        {/* 
+            <FrequencyChart elements={["F", "F", "F", "F", "F", "F", "F", "D", "D", "D", "D", "D", "C", "C", "B", "B", "B", "B", "B", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A", "A",]} />
+        */}
     </body>
 }
---
+++ web_server/code/systems/chartjs.js
@@ -0,0 +1,7 @@
+const { Chart, ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Decimation, Filler, Legend, Title, Tooltip, SubTitle } = require("chart.js")
+// register all the basics
+Chart.register(ArcElement, LineElement, BarElement, PointElement, BarController, BubbleController, DoughnutController, LineController, PieController, PolarAreaController, RadarController, ScatterController, CategoryScale, LinearScale, LogarithmicScale, RadialLinearScale, TimeScale, TimeSeriesScale, Decimation, Filler, Legend, Title, Tooltip, SubTitle)
+
+module.exports = {
+    Chart
+}
