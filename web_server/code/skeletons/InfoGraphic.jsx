deadb4883505680865bc8307dc7855722c3d2ddc
Jeff Hykin <jeff.hykin@gmail.com>
2021-10-08 11:56:58 -0500

    scrolling works

--- web_server/code/skeletons/InfoGraphic.jsx
+++ web_server/code/skeletons/InfoGraphic.jsx
@@ -4,14 +4,16 @@ module.exports = ({ children, ...properties }) => {
     let wrapper
     let isHovered = false
     onScroll((event)=>{
-        if (wrapper.isHovered) {
-            console.log(`event is:`,event)
+        if (wrapper && wrapper.isHovered) {
+            console.log(`wrapper.style.bottom is:`,wrapper.style.bottom)
+            window.wrapper = wrapper
+            wrapper.style.bottom = -(event.deltaY) + (wrapper.style.bottom.replace(/px/,"")-0)
         }
     })
     return wrapper = <div
         name="info-graphic"
         // class="animate"
-        style="position: absolute; bottom: 0; transform: translateY(-100%); width: 100%; z-index: 999;"
+        style="position: absolute; bottom: 0; transform: translateY(-100%); width: 100%; z-index: 999; transition: all 0.01s ease-in-out 0s;"
         onscroll={(e=>console.log(e))}
         onmouseover={()=>wrapper.isHovered=true}
         onmouseenter={()=>wrapper.isHovered=true}
