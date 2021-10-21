937dd2a98a2ab6d46f42c45fced863afda99e26e
Anthony <yihonganthony@gmail.com>
2021-10-20 23:51:58 -0500

    Added reusable tree component with data structure

--- web_server/code/pages/Home.jsx
+++ web_server/code/pages/Home.jsx
@@ -1,6 +1,66 @@
 require("../systems/on_scroll")
 const GraphDisplay = require("../skeletons/GraphDisplay")
+const BaseTree = require("../skeletons/BaseTree")
 const InfoGraphic = require("../skeletons/InfoGraphic")
+
+const treeData = 
+  {
+    name: 'topLevel',
+    parent: 'null',
+    blurb: 10,
+    type: 'black',
+    level: 'black',
+    children: [
+      {
+        name: 'midLevel',
+        parent: 'topLevel',
+        blurb: 5,
+        type: 'black',
+        level: 'none',
+        children: [
+          {
+            name: 'lowA',
+            parent: 'midLevel',
+            blurb: 5,
+            type: 'Type of bug',
+            level: 'red'
+          },
+          {
+            name: 'lowB',
+            parent: 'midLevel',
+            blurb: 18,
+            type: 'Type of vulnerability',
+            level: 'red'
+          }
+        ]
+      },
+      {
+        name: 'midLevelB',
+        parent: 'topLevel',
+        blurb: 10,
+        type: 'grey',
+        level: 'none',
+        children: [
+          {
+            name: 'lowC',
+            parent: 'midLevelB',
+            blurb: 5,
+            type: 'Type of vulnerability',
+            level: 'red'
+          },
+          {
+            name: 'lowD',
+            parent: 'midLevelB',
+            blurb: 18,
+            type: 'Type of vulnerability',
+            level: 'red'
+          }
+        ]
+      }
+    ]
+  }
+
+const org = [treeData, treeData];
 
 module.exports = ({ children, ...properties }) => {
     let thing
@@ -14,20 +74,16 @@ module.exports = ({ children, ...properties }) => {
     //     // thing.addEventListener("onsscroll", listener)
     //     // thing.addEventListener("sscroll", listener)
     // }, 2000)
+
+    let orgSelected = false;
+    let treeSelected = null;
     return <body class="column centered" style="height: 100vh; overflow: hidden;">
-        
-        <GraphDisplay sizeOfNodeInPixels={300} padding={200}>
-            <h4>Howdy1</h4>
-            <h4>Howdy2</h4>
-            <h4>Howdy3</h4>
-            <h4>Howdy4</h4>
-            <h4>Howdy5</h4>
-            <h4>Howdy6</h4>
-            <h4>Howdy7</h4>
-            <h4>Howdy8</h4>
-        </GraphDisplay>
-        
-        
+        {orgSelected && treeSelected ? <BaseTree treeData={treeData}/> : orgSelected ? <div>
+        {org.map((tree, i) => <div style={`position: absolute; transform: translate(${i * 100 / org.length}%,${i % 2 * -50}px)`}><BaseTree treeData={tree}/></div>)}
+        </div> : <div className="orgBubble">
+            {org.map((tree, i) => {
+                  return <div style={`position: absolute; transform: translate(${i * 100 / org.length}%,${i % 2 * -50}px); pointer-events: none;`}><BaseTree treeData={tree}/></div>
+            })}</div>}
         <InfoGraphic>
             <div class="centered" style="width: 100%; transform: scale(4)" >
                 {thing = <div style="height: 1rem; width: 2rem; overflow: scroll; background-color: red;">
