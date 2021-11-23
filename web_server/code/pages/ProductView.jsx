71ef2fa08c4174a6e0bcbd2f4a9bcc5bda22e891
Jeff Hykin <jeff.hykin@gmail.com>
2021-11-23 10:59:57 -0600

    add left and right side to product view

--- web_server/code/pages/ProductView.jsx
+++ web_server/code/pages/ProductView.jsx
@@ -1,5 +1,20 @@
-module.exports = ({ children, title, actions=[], ...properties }) => {
-    return <main>
-        
+const { watch } = require("@vue-reactivity/watch")
+const router = require("quik-router")
+
+module.exports = ({ ...properties }) => {
+    const whichRepo = router.pageInfo.repoName
+    return <main
+        class="centered row"
+        style={`
+            width: 100%;
+            height: 100%;
+        `}
+        >
+        <div name="left" class="centered" style="width: 50%; height: 100%;">
+            Left Side
+        </div>
+        <div name="right" class="centered" style="width: 50%; height: 100%; background: rgba(0, 0, 0, 0) radial-gradient(circle, rgb(243, 243, 243) 0%, rgba(236, 236, 236, 0.9) 100%) repeat scroll 0% 0%;">
+            Right Side
+        </div>
     </main>
 }
