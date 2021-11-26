984e0fb3366db2430f784942df84b07688b3c0e0
Jeff Hykin <jeff.hykin@gmail.com>
2021-11-26 15:55:49 -0600

    update data processing methods

--- data_processing/commits_to_yaml.js
+++ data_processing/commits_to_yaml.js
@@ -3,28 +3,34 @@ const yaml = require('js-yaml')
 const path = require('path')
 const { argv } = require("process")
 const { snakeCase } = require("snake-case")
+const process = require("process")
 
-const whichFile = argv[2]
-let fileAsString = fs.readFileSync(whichFile).toString()
+const commitLogsFolder = path.join(process.env.FORNIX_FOLDER, "data", "commit_logs")
+for (const whichFileName of fs.readdirSync(commitLogsFolder)) {
+    if (whichFileName.match(/.txt$/g)) {
+        const whichFile = path.join(commitLogsFolder, whichFileName)
+        let fileAsString = fs.readFileSync(whichFile).toString()
 
-// remove non printable characters
-fileAsString = fileAsString.replace(/[^ -~\n ]+/g, "");
-// make messages have keys
-fileAsString = fileAsString.replace(/((\n|^)Date:.+)/g, "$1\nMessage: |")
-// indent everything
-fileAsString = fileAsString.replace(/(\n|^)(.+)/g, "$1    $2")
-// convert commits to be keys
-fileAsString = fileAsString.replace(/(\n|^)    commit (.+)/g, "$1$2:")
-// replace "bad" indentation
-fileAsString = fileAsString.replace(/(\n|^)        +/g, "        ")
-// double indent everything that doesn't have a colon
-fileAsString = fileAsString.replace(/(\n|^)(    [^: ][^:\n]*)(?=\n)/g, "$1    $2")
+        // remove non printable characters
+        fileAsString = fileAsString.replace(/[^ -~\n ]+/g, "");
+        // make messages have keys
+        fileAsString = fileAsString.replace(/((\n|^)Date:.+)/g, "$1\nMessage: |")
+        // indent everything
+        fileAsString = fileAsString.replace(/(\n|^)(.+)/g, "$1    $2")
+        // convert commits to be keys
+        fileAsString = fileAsString.replace(/(\n|^)    commit (.+)/g, "$1$2:")
+        // replace "bad" indentation
+        fileAsString = fileAsString.replace(/(\n|^)        +/g, "        ")
+        // double indent everything that doesn't have a colon
+        fileAsString = fileAsString.replace(/(\n|^)(    [^: ][^:\n]*)(?=\n)/g, "$1    $2")
 
 
-const fileName = path.join(path.dirname(whichFile), snakeCase(path.basename(whichFile).replace(/.txt$/, "")))
-// save yaml file
-fs.writeFileSync(fileName+".yaml", fileAsString)
+        const fileName = path.join(path.dirname(whichFile), snakeCase(path.basename(whichFile).replace(/.txt$/, "")))
+        // save yaml file
+        fs.writeFileSync(fileName+".yaml", fileAsString)
 
-// save the json file
-const parsedCommitData = yaml.load(fileAsString)
-fs.writeFileSync(fileName+".json", JSON.stringify(parsedCommitData,0,4))
+        // save the json file
+        const parsedCommitData = yaml.load(fileAsString)
+        fs.writeFileSync(fileName+".json", JSON.stringify(parsedCommitData,0,4))
+    }
+}
--- data_processing/convert_vulns_csv.js
+++ data_processing/convert_vulns_csv.js
@@ -2,10 +2,76 @@
 const fs = require("fs")
 const request = require('request')
 const _ = require("lodash")
+const parse = require("csv-parse/lib/sync")
+
+
+// 
+// CSV to JSON
+//
+// (failed because csv-parse cant handle strings with newlines) 
+// const string = fs.readFileSync("./data/vulnerabilities.csv").toString()
+// var fixedString = string.replace(/,[\n \t]*"[\w\W]*?[\w\W]*?"[\n \t]*(,|\n)/mg, each=>each.replace(/\n/g, "")).replace(/\r/,"")
+// fs.writeFileSync("./data/vulnerabilities_fixed.csv", fixedString)
+// var csvStringLines = fixedString.split("\n")
+// var correctNumberOfCommas = csvStringLines[0].match(/,/g).length
+// var index = 0
+// var fixedCsvLines = csvStringLines.map(each=>{
+//     console.log(`   (${(index/csvStringLines.length).toFixed(3)}) ${csvStringLines.length}:${index++}`)
+//     if (each.match(/"/g)) {
+//         const before = each
+//         each = each.replace(/""/g,'').replace(/"[^"]+"/g, "") // .replace(/'([^\\].|[^'])+?'/g, "")
+//         if (each.match(/"/g)) {
+//             console.log("        there were still quotes leftover")
+//             console.log(`            before: ${before}`)
+//             console.log(`            after : ${each}`)
+//             throw Error(``)
+//         }
+//     }
+//     // const each = each.replace(/".+?"/g, "").replace(/'([^\\].|[^'])+'/g, "")
+//     const commas = each.match(/,/g)
+//     if (commas == null) {
+//         return ""
+//     }
+//     const numberOfCommas = commas.length
+//     let extension = ""
+//     if (numberOfCommas < correctNumberOfCommas) {
+//         extension = ",".repeat(correctNumberOfCommas-numberOfCommas)
+//     }
+//     return each+extension
+// })
+// console.log(fixedCsvLines.slice(0,90))
+// fs.writeFileSync("./data/vulnerabilities.json", JSON.stringify(
+//     parse(fixedCsvLines.join("\n"), {
+//         columns: true,
+//         skip_empty_lines: true,
+//     })
+// ))
 
 
 const records = require("../data/vulnerabilities.json")
 
+// 
+// remove descriptions (otherwise file is > 100mb)
+// 
+fs.writeFileSync("./data/vulnerabilities.json", JSON.stringify(records.map(each=>{
+    const copy = {...each}
+    delete copy.Desc
+    return copy
+})))
+
+// 
+// create a seperate file for descriptions
+// 
+let descriptions = {}
+for (const each of records) {
+    descriptions[each["CVE ID"]] = each.Desc
+}
+fs.writeFileSync("./data/vulnerability_descriptions.json", JSON.stringify(descriptions,0,4))
+
+
+// 
+// JSON to pandas-dataframe-friendly JSON
+// 
 const totals = {
     cveId:[],
     cweId:[],
@@ -42,4 +108,4 @@ records.map((object)=>{
     totals.vendor.push(object["Vendor"])
     totals.product.push(object["Product"])
 })
-fs.writeFileSync("vulnerabilities_array.json", JSON.stringify(totals))
+fs.writeFileSync("vulnerabilities_array.json", JSON.stringify(totals,0,4))
