8883fa17589516785d21275e88e604e7d417d9d9
Jeff Hykin <jeff.hykin@gmail.com>
2021-11-26 17:59:33 -0600

    improve mobile view

--- web_server/code/skeletons/Waterfall.jsx
+++ web_server/code/skeletons/Waterfall.jsx
@@ -5,8 +5,8 @@ const SquareGrid = require("./SquareGrid")
 // 
 module.exports = ({ children })=>{
     const hasReallyNarrowScreen = window.innerWidth / window.innerHeight < 0.9
-    return <div name="waterfall-outermost" style="width: 100%; min-height: 100%; overflow-x: hidden; overflow-y: auto; display: flex; align-content: center; justify-content: center; justify-content: center; background: var(--soft-gray-gradient);">
-        <SquareGrid numberOfSquares={hasReallyNarrowScreen ? 6 : 12} style="width: 80rem; max-width: 100%; height: 100%; min-height: fit-content; padding: 2rem; box-sizing: border-box;">
+    return <div name="waterfall-outermost" style={`width: 100%; min-height: 100%; overflow-x: hidden; overflow-y: auto; display: flex; align-content: center; justify-content: center; justify-content: center; background: var(--soft-gray-gradient); ${hasReallyNarrowScreen&&'font-size: max(1em, 20pt)'}`}>
+        <SquareGrid numberOfSquares={hasReallyNarrowScreen ? 7 : 12} style="width: 80rem; max-width: 100%; max-width: 80vw; height: 100%; min-height: fit-content; padding: 2rem; box-sizing: border-box;">
             {children}
         </SquareGrid>
     </div>
