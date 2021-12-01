01923945a6cbbd70e5855227f5c77abb6e46ac34
Jeff Hykin <jeff.hykin@gmail.com>
2021-12-01 12:45:36 -0600

    add ffmpeg git commits

--- data_processing/gather_commits.sh
+++ data_processing/gather_commits.sh
@@ -17,14 +17,16 @@ cd "$git_repo_name"
 log_data_filepath="log_data.broken.json"
 # generate log data for it
 # check if file exists
+unique_identifier="@#20498@2308423#@"
 if ! [ -f "$log_data_filepath" ]
 then
+    true
     echo "generating log"
-    git log --stat --branches="*" --pretty=format:'%n^@^^@^Start^@^^@^%n{%n^@^hash^@^:^@^%h^@^,%n^@^author^@^:^@^%an^@^,%n^@^date^@^:^@^%ad^@^,%n^@^email^@^:^@^%aE^@^,%n^@^message^@^:^@^%s^@^,%n^@^commitFullDate^@^:^@^%ai^@^,%n^@^age^@^:^@^%cr^@^%n},^@^^@^End^@^^@^%n' > "$log_data_filepath" || exit
+    git log --stat --branches="*" --pretty=format:"%n${unique_identifier}${unique_identifier}Start${unique_identifier}${unique_identifier}%n{%n${unique_identifier}hash${unique_identifier}:${unique_identifier}%h${unique_identifier},%n${unique_identifier}author${unique_identifier}:${unique_identifier}%an${unique_identifier},%n${unique_identifier}date${unique_identifier}:${unique_identifier}%ad${unique_identifier},%n${unique_identifier}email${unique_identifier}:${unique_identifier}%aE${unique_identifier},%n${unique_identifier}message${unique_identifier}:${unique_identifier}%s${unique_identifier},%n${unique_identifier}commitFullDate${unique_identifier}:${unique_identifier}%ai${unique_identifier},%n${unique_identifier}age${unique_identifier}:${unique_identifier}%cr${unique_identifier}%n},${unique_identifier}${unique_identifier}End${unique_identifier}${unique_identifier}%n" > "$log_data_filepath" || exit
 fi
 # use node to process it
 echo "formatting log"
-new_file="$(node --max-old-space-size=8192 "$FORNIX_FOLDER/data_processing/format_commit_data.js" "$log_data_filepath")"
+new_file="$(node --max-old-space-size=8192 "$FORNIX_FOLDER/data_processing/format_commit_data.js" "$log_data_filepath" "$unique_identifier")"
 echo "moving file"
 final_filepath="$FORNIX_FOLDER/data/full_commit_logs/$git_repo_name.json"
 rm -f "$final_filepath"
