743757bbf757e42036f9e408738c822bf49f98fb
Jeff Hykin <jeff.hykin@gmail.com>
2021-12-01 00:35:45 -0600

    -

--- web_server/code/data/fullCommitLogFor.backend.js
+++ web_server/code/data/fullCommitLogFor.backend.js
@@ -4,25 +4,30 @@ const nameConversion = require("../../../data/product_to_repo_names.json")
 const cachedData = new Map()
 
 module.exports = async ({product})=>{
-    const repoName = nameConversion[product]
-    if (!repoName) {
-        return null
+    try {
+        const repoName = nameConversion[product]
+        if (!repoName) {
+            return null
+        }
+        if (!cachedData.has(repoName)) {
+            const location = path.join("data",'full_commit_logs',repoName+".json")
+            const stringValue = await fs.readFile(location)
+            const objectValue = JSON.parse(stringValue).map(each=>{
+                // convert from compressed list form
+                const [ filesChanged, linesChanged, commitDate,] = each
+                // to decompressed object form (depends on the format of data_processing/format_commit_data.js)
+                return {
+                    filesChanged, linesChanged, commitDate,
+                }
+            })
+            cachedData.set(repoName, objectValue)
+        }
+        const returnValue = cachedData.get(repoName)
+        // un-cache because the server has so little ram
+        cachedData.delete(repoName)
+        return returnValue
+    } catch (error) {
+        console.debug(`error is:`,error)
+        throw error
     }
-    if (!cachedData.has(repoName)) {
-        const location = path.join("data",'full_commit_logs',repoName+".json")
-        const stringValue = await fs.readFile(location)
-        const objectValue = JSON.parse(stringValue).map(each=>{
-            // convert from compressed list form
-            const [ filesChanged, linesChanged, commitDate,] = each
-            // to decompressed object form (depends on the format of data_processing/format_commit_data.js)
-            return {
-                filesChanged, linesChanged, commitDate,
-            }
-        })
-        cachedData.set(repoName, objectValue)
-    }
-    const returnValue = cachedData.get(repoName)
-    // un-cache because the server has so little ram
-    cachedData.delete(repoName)
-    return returnValue
 }
